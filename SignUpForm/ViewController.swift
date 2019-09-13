47a13a41993e146529ae8fe2e3be0ddb4f92e1da
Eddie <eahnivy8@gmail.com>
2019-09-13 12:32:28 +0900

    SignUp form completed.

--- SignUpForm/ViewController.swift
+++ SignUpForm/ViewController.swift
@@ -1,20 +1,39 @@
-//
-//  ViewController.swift
-//  SignUpForm
-//
-//  Created by Eddie Ahn on 12/09/2019.
-//  Copyright © 2019 Eddie Ahn. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var telTextField: UITextField!
+    @IBOutlet var blogTextField: UITextField!
+    @IBOutlet var textField: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+        
+        
     }
-
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        nameTextField.resignFirstResponder()
+//        emailTextField.resignFirstResponder()
+//        passwordTextField.resignFirstResponder()
+//        telTextField.resignFirstResponder()
+//        blogTextField.resignFirstResponder()
+        textField.resignFirstResponder()
+        return true
+    }
+    @IBAction func signUpAction(_ sender: Any) {
+        textField.text! = "Welcome to our website \(nameTextField.text!)."
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+self.view.endEditing(true)
+    }
+   
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+}
